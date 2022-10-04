a51f6c9c025654bd0eb9e438f0c1a19e8ec02b39
hezonglianheng <976410976@qq.com>
2022-10-04 15:09:02 +0800

    2022/10/4 第三次提交

--- mark_1.py
+++ mark_1.py
@@ -19,13 +19,9 @@ def main(corpus_file, result_file):
         for i in range(len(corpus)):
             print(corpus[i])
             legal = input(
-                '合法性判定（0，非法；1，合法；q，退出标注）：'
+                '合法性判定（0，非法；1，合法；2，不确定；x，不适用；q，退出标注）：'
             )
-            if legal == '0' or legal == '1':
-                result_writer.writerow(
-                    [corpus[i], legal]
-                )
-            elif legal == 'q':
+            if legal == 'q':
                 word = input('标注词：')
                 now = str(date.today())
                 file_name = './corpus/' + word + now + '.txt'
@@ -35,5 +31,9 @@ def main(corpus_file, result_file):
                     f.writelines(corpus[i:])
                 logging.info('本次标注了{}条文本。'.format(i))
                 return None
+            else:
+                result_writer.writerow(
+                    [corpus[i], legal]
+                )
 
     logging.info('标注完成！')
